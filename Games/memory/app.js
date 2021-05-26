3b3ebb5f6bd24e6baef3411d5e587791e0012128
KeikeiSo <calliasu2018@gmail.com>
2021-05-26 16:57:22 -0400

    Initial commit

---
+++ Games/memory/app.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // card options
+    const cardArray = [
+        {
+            name: 'Bulbasaur',
+            img: 'images/Bulbasaur.png'
+        },
+
+        {
+            name: 'Charmander',
+            img: 'images/Charmander.png'
+        },
+
+        {
+            name: 'Chikoriita',
+            img: 'images/Chikoriita.png'
+        },
+
+        {
+            name: 'Firebird',
+            img: 'images/Firebird.png'
+        },
+
+        {
+            name: 'Mudkip',
+            img: 'images/Mudkip.png'
+        },
+
+        {
+            name: 'Squirtle',
+            img: 'images/Squirtle.png'
+        },
+
+        {
+            name: 'Bulbasaur',
+            img: 'images/Bulbasaur.png'
+        },
+
+        {
+            name: 'Charmander',
+            img: 'images/Charmander.png'
+        },
+
+        {
+            name: 'Chikoriita',
+            img: 'images/Chikoriita.png'
+        },
+
+        {
+            name: 'Firebird',
+            img: 'images/Firebird.png'
+        },
+
+        {
+            name: 'Mudkip',
+            img: 'images/Mudkip.png'
+        },
+
+        {
+            name: 'Squirtle',
+            img: 'images/Squirtle.png'
+        }
+    ]
+
+    const grid = document.querySelector('.grid')
+
+    // create game board
+    
+})
---
+++ basic/index.js
@@ -0,0 +1,24 @@
+// print hello world
+console.log('Hello World');
+const name = "kei";
+let c = 10;
+
+let person = {
+    name: 'Callia',
+    age: 19
+};
+
+person.name = 'LOL';
+
+let selection = 'name';
+person[selection] = 'Mary';
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 10;
+console.log(selectedColors);
+
+function greet() {
+    console.log('Hello World Again!')
+}
+
+greet();
